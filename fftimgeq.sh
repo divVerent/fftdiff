5d53722dedd0c6b069ec313043cfb4ddd60f8348
Rudolf Polzer <divverent@xonotic.org>
2013-04-14 09:30:29 +0200

    fix sox use

--- fftimgeq.sh
+++ fftimgeq.sh
@@ -10,4 +10,4 @@ sox "$a" -e signed -b 16 -c 1 1.raw
 geometry=`./fftimgeq 1.raw /dev/null 2.raw 2>&1 | awk '/^Total image size:/ { print $4 }'`
 convert "$i" -flip -transpose -geometry "$geometry"! -depth 8 GRAY:- |\
 	./fftimgeq 1.raw /dev/stdin 2.raw
-sox -t raw -e signed -b 16 -c 1 2.raw -r "$r" "$b"
+sox -t raw -e signed -b 16 -c 1 -r "$r" 2.raw "$b"
